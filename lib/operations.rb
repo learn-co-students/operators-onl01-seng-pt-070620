2a23e8925204df4bcf90a7d3f693c840c386844a
Denise Henderson <denise@henderson3.us>
2020-07-08 14:39:34 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  if speed<40 || speed>60
+    notsafe = true
+  else
+    notsafe = false
+  end
+  return notsafe
 end
-
-
 
 def not_safe?(speed)
-	
+  speed<40||speed>60?notsafe=true:notsafe=false
+  return notsafe
 end
-	
-
-
