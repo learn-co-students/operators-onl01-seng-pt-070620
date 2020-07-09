bb07cc0487542151935d586b266f542a53f28413
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-09 12:11:36 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-
+if speed>40 && speed <60
+ false
+else speed <40 || speed >60
+ true
 end
-
-
+end
+speed=55
 
 def not_safe?(speed)
-	
+ speed >40 && speed<60? false:true
 end
-	
-
-
